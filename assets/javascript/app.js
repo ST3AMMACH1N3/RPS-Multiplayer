a5d78bbc7d32b0e3adcba4c51ae7128d312bb0a7
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-04 14:36:00 -0400

    Finished the html for the starting page, put the styling in the reset css, added the firebase confige to the app.js, and added the images to be used for the Rock Paper and Scissors

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyAZHgntVV2i5prpRULYP11bceh_s_ghvyc",
+    authDomain: "rps-multiplayer-3557e.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-3557e.firebaseio.com",
+    projectId: "rps-multiplayer-3557e",
+    storageBucket: "rps-multiplayer-3557e.appspot.com",
+    messagingSenderId: "402935998812"
+};
+firebase.initializeApp(config);
