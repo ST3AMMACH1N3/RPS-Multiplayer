e11eaba5f404cde5024b614d0ec38892fc046b90
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-09 15:58:52 -0400

    Added a sign in function which check if there is room to fit you in, if not adds you to the viewer list, added the findNextPlayer function which finds the next player in line in the viewer list and has them join the game, added the joinGame function which adds the player to the player list of the database.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,6 +17,51 @@ $(document).ready(() => {
     let players = [];
     let viewers = [];
 
+    //Finds the next player in line to player
+    function findNextPlayer() {
+        //Return the first player in the viewers section
+        database.ref("viewers").limitToFirst(1).once("value", (snapshot) => {
+            //Access the snapshot at the key
+            for (let key in snapshot.val()) {
+                //If the next player in line is me
+                if (snapshot.val()[key].name === myName) {
+                    let reference = `viewers/${key}`
+                    //Remove me from the viewers list
+                    database.ref(reference).remove();
+                    //Join the game
+                    joinGame()
+                }
+            }
+        });
+    }
+
+    //Add me to the database
+    function joinGame() {
+        database.ref("players").push({
+            name: myName,
+            move: "none",
+            wins: 0
+        });
+    }
+
+    //Sign me into the database, if there is a spot open, add me, otherwise add me to the viwers
+    function signIn() {
+        //If there are no players or there is one that is not me
+        if (players.length === 0 || (players.length === 1 && players[0].name !== myName)) {
+            //If I am already a viewer find the next player otherwise try to join the game
+            if (viewers.indexOf(myName) !== -1) {
+                findNextPlayer();
+            } else {
+                joinGame();
+            }
+        //If there are 2 players add me to the viewers list
+        } else if (players.length === 2) {
+            database.ref("viewers").push({
+                name: myName,
+            })
+        }
+    }
+
     //If my the box is not empty set the variable myName equal to the text in the box
     $("#sign-in-btn").on("click", () => {
         myName = $("#name-input").val().trim();
