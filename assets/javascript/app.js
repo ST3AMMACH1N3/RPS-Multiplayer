7153b01b00209deac301ab356a2bc968396904ce
Ben Houston <BenjaminAHouston@gmail.com>
2018-10-09 15:54:56 -0400

    Added the document ready function added local variables to keep track of things locally, made an on click function that sets your local name if it's not an empty string

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,30 @@
-let config = {
+$(document).ready(() => {
+    //Set up firebase for use
+    let config = {
         apiKey: "AIzaSyAZHgntVV2i5prpRULYP11bceh_s_ghvyc",
         authDomain: "rps-multiplayer-3557e.firebaseapp.com",
         databaseURL: "https://rps-multiplayer-3557e.firebaseio.com",
         projectId: "rps-multiplayer-3557e",
         storageBucket: "rps-multiplayer-3557e.appspot.com",
         messagingSenderId: "402935998812"
-};
-firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
+
+    let database = firebase.database();
+
+    //Set up variables to keep track of things locally
+    let myName;
+    let players = [];
+    let viewers = [];
+
+    //If my the box is not empty set the variable myName equal to the text in the box
+    $("#sign-in-btn").on("click", () => {
+        myName = $("#name-input").val().trim();
+        $("#name-input").val("");
+        if (myName !== "") {
+            signIn();
+        } else {
+            myName = "";
+        }
+    });
+});
